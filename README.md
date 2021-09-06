c23d112a1b960fbd4f7c4d65159c10664755e3a1
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2021-09-05 19:54:19 -0700

    basic sdl project running

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # boidsSDL
+
+## Dependencies
+
+g++, CMake, and SDL2
+
+## Running the code
+
+Once all dependencies are installed, go to the build subdirectory and run
+```
+cmake ..
+```
+and then, once cmake has completed, 
+```
+make
+``` 
+can be run from the bin or build directories, with an output in /bin which can be run with
+```
+./boids
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "SDL.h"
 
 int main(){
-    std::cout<<"Hello World!\n";
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow(
+        "boids",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0
+    );
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_Delay(3000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
